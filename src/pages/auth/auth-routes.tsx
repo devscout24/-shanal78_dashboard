4191634a858d78090a0dbd53317550e436ed91ef
Rashad-Stack <tex.eng.rashed@gmail.com>
2026-08-01 14:11:00 +0600

    refactor: remove unused AuthRoutes component and related imports

--- src/pages/auth/auth-routes.tsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/config/firebase";
-import { Navigate, Outlet } from "react-router";
-
-export default function AuthRoutes() {
-  const user = auth.currentUser;
-
-  return !user ? <Outlet /> : <Navigate to="/dashboard" replace={true} />;
-}
